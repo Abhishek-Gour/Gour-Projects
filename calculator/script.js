abb0c0510a80cd33c3257523b5931e18abb0f6bc
ABHISHEK GOUR <gourabhishek026@gmail.com>
2023-02-14 22:02:45 +0530

    Working is progress.

--- calculator/script.js
+++ calculator/script.js
@@ -1,9 +1,19 @@
-let string = '';
+let string = "";
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button) => {
     button.addEventListener('click', (e) => {
-        console.log(e.target);
-        string = string + e.target.innerHTML;
-        document.querySelectorAll('input').value = string;
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelectorAll('input').value = string;
+        }
+        else if(e.target.innerHTML == 'c') {
+            string = "";
+            document.querySelectorAll('input').value = string;
+        }
+        else {
+            console.log(e.target);
+            string = string + e.target.innerHTML;
+            document.querySelectorAll('input').value = string;
+        }
     })
 })
